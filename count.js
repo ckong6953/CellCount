75d565c0d13c1e582365dbbefdd96aba8a00aba9
Chris <ckong6953@gmail.com>
2021-08-09 01:16:03 -0400

    Dynamic Tables made

--- count.js
+++ count.js
@@ -1,8 +1,10 @@
+let baseVolume = [];
+
 class FormSubmit extends React.Component {
     constructor (props){
         super(props);
         this.state = {
-            value: "",
+            value: 1,
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -14,7 +16,13 @@ class FormSubmit extends React.Component {
     }
 
     handleSubmit(event){
-        ReactDOM.render(<CreateTable amount={this.state.value} />, document.getElementById("count-table"))
+        let tables = []
+        tables.push(<p key="instructions">Please fill out the sample name as well as the respective dilution factor and observed raw cell counts in the fields below. These are utilized to calculate the average counts as well as the amount of cells found per volume in milliliters. If fields are failing tp update, please press enter after entering a field value. Once finished, please click the "Submit Table(s)" button:</p>)
+        for (let i = 0; i < this.state.value; i++){
+            const tableId = "table-"+i
+            tables.push(<CreateTable key={tableId} tableID={i}/>);
+        }
+            ReactDOM.render(<div>{tables}</div>, document.getElementById("count-table"));
         event.preventDefault();
     }
 
@@ -32,113 +40,177 @@ class FormSubmit extends React.Component {
     }
 }
 
-
 class CreateTable extends React.Component {
     constructor (props){
         super (props);
         this.state = {
+            tableTitle: '',
             countOne: 0,
             countTwo: 0,
             countThree: 0,
             countFour: 0,
-            dilFac: 0,
             avgCount: 0,
+            dilFac: 0,
+            cellVolume: 0,
         }
 
         this.handleDilution = this.handleDilution.bind(this);
-        this.handleOneChange = this.handleOneChange.bind(this);
-        this.handleTwoChange = this.handleTwoChange.bind(this);
-        this.handleThreeChange = this.handleThreeChange.bind(this);
-        this.handleFourChange = this.handleFourChange.bind(this);
+        this.handleOne = this.handleOne.bind(this);
+        this.handleTwo = this.handleTwo.bind(this);
+        this.handleThree = this.handleThree.bind(this);
+        this.handleFour = this.handleFour.bind(this);
+        this.handleAverage = this.handleAverage.bind(this);
+        this.handleVolume = this.handleVolume.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleTitleSave = this.handleTitleSave.bind(this);
+    }
+
+    handleTitleChange(event){
+        this.setState({tableTitle: event.target.value});
     }
 
     handleDilution(event){
-        this.setState({dilFac: event.target.value})
+        this.setState({dilFac: event.target.value});
     }
 
-    handleOneChange(event){
-        this.setState({countOne: event.target.value})
+    handleOne(event){
+        this.setState({countOne: event.target.value});
     }
 
-    handleTwoChange(event){
-        this.setState({countTwo: event.target.value})
+    handleTwo(event){
+        this.setState({countTwo: event.target.value});
     }
 
-    handleThreeChange(event){
-        this.setState({countThree: event.target.value})
+    handleThree(event){
+        this.setState({countThree: event.target.value});
     }
 
-    handleFourChange(event){
+    handleFour(event){
         this.setState({countFour: event.target.value});
     }
-    
+
+    handleAverage(event){
+        const newAvg = Number(parseInt(this.state.countOne) + parseInt(this.state.countTwo) + parseInt(this.state.countThree) + parseInt(this.state.countFour))/4;
+        const newVolume = Number.parseFloat(newAvg * this.state.dilFac * 10000).toExponential(2);
+        this.setState({avgCount: newAvg,
+        cellVolume: newVolume});
+        event.preventDefault();
+    }
+
+    handleVolume(event){
+        event.preventDefault();
+    }
+
+    handleTitleSave(event){
+        event.preventDefault();
+    }
+
+
     render(){
-        let tableList = [];
-        let rows = []
-        for (let i = 0; i < 6; i++){
+        let rows = [];
+        for (let i = 0; i < 7; i++){
             const rowID = "row-"+i;
             let cells = [];
             for (let j = 0; j < 2; j++){
-                const cellID = "cell-" + i + "-" + j;
-                switch (j){
+                const cellID = "cell-"+i+"-"+j;
+                switch(j){
                     case 0:
-                        switch (i){
+                        switch(i){
                             case 0:
                                 cells.push(<td key={cellID}>Dilution Factor</td>);
                                 break;
                             case 1:
-                                cells.push(<td key={cellID}>Raw Count 1</td>);
+                                cells.push(<td key={cellID}>Count 1</td>)
                                 break;
                             case 2:
-                                cells.push(<td key={cellID}>Raw Count 2</td>);
+                                cells.push(<td key={cellID}>Count 2</td>)
                                 break;
                             case 3:
-                                cells.push(<td key={cellID}>Raw Count 3</td>);
+                                cells.push(<td key={cellID}>Count 3</td>)
                                 break;
                             case 4:
-                                cells.push(<td key={cellID}>Raw Count 4</td>);
+                                cells.push(<td key={cellID}>Count 4</td>)
                                 break;
                             case 5:
-                                cells.push(<td key={cellID}>Average</td>);
+                                cells.push(<td key={cellID}>Average</td>)
+                                break;
+                            case 6:
+                                cells.push(<td key={cellID}>Cell/mL</td>)
+                                break;
                         }
                         break;
                     case 1:
                         switch(i){
-                        case 0:
-                            cells.push(<td key={cellID}>
-                                <form> <input type="number" placeholder="0" min="0" value={this.state.dilFac}
-                                onChange={this.handleDilution}/> </form>
+                            case 0:
+                                cells.push(<td key={cellID}>
+                                    <form onSubmit={this.handleAverage  }>
+                                        <input type="number" placeholder="0" min="0" value={this.state.dilFac} onChange={this.handleDilution}
+                                        />
+                                    </form>
                                 </td>);
-                            break;
-                        case 1:
-                            cells.push(<td key={cellID}><input type="number" placeholder="0" min="0" value={this.state.countOne} onChange={this.handleOneChange}/></td>);
-                            break;  
-                        case 2:
-                            cells.push(<td key={cellID}><input type="number" placeholder="0" min="0" value={this.state.countTwo} onChange={this.handleTwoChange}/></td>) ;
-                            break;
-                        case 3: 
-                            cells.push(<td key={cellID}><input type="number" placeholder="0" min="0" value={this.state.countThree} onChange={this.handleThreeChange}/></td>)
-                            break;
-                        case 4: 
-                            cells.push(<td key={cellID}><input type="number" placeholder="0" min="0" value={this.state.countFour} onChange={this.handleFourChange}/></td>)  
-                            break;
-                        case 5:
-                            cells.push(<td key={cellID}>{this.state.avgCount}</td>)  
-                        }      
+                                break;
+                            case 1:
+                                cells.push(<td key={cellID}>
+                                    <form onSubmit={this.handleAverage}>
+                                        <input type="number" placeholder="0" min="0" value={this.state.countOne} onChange={this.handleOne}
+                                        />
+                                    </form>
+                                </td>);
+                                break;
+                            case 2:
+                                cells.push(<td key={cellID}>
+                                    <form onSubmit={this.handleAverage}>
+                                        <input type="number" placeholder="0" min="0" value={this.state.countTwo} onChange = {this.handleTwo}
+                                        />
+                                    </form>
+                                </td>);
+                                break;
+                            case 3:
+                                cells.push(<td key={cellID}>
+                                    <form onSubmit={this.handleAverage}>
+                                        <input type="number" placeholder="0" min="0" value={this.state.countThree} onChange = {this.handleThree}
+                                        />
+                                    </form>
+                                </td>);
+                                break;
+                            case 4:
+                                cells.push(<td key={cellID}>
+                                    <form onSubmit={this.handleAverage}>
+                                        <input type="number" placeholder="0" min="0" value={this.state.countFour} onChange={this.handleFour}
+                                        />
+                                    </form>
+                                </td>);
+                                break;
+                            case 5:
+                                cells.push(<td key={cellID}>{this.state.avgCount}</td>);
+                                break;
+                            case 6:
+                                cells.push(<td key={cellID}>{this.state.cellVolume}</td>)
+                        }
                 }
             }
             rows.push(<tr key={rowID}>{cells}</tr>);
         }
 
-        for (let i = 0; i < this.props.amount; i++){
-            const tableID = "table-" + i;
-            const headerID = "tableHeader-" + i;
-            tableList.push(<table key={tableID}>
-                <thead key={headerID}><tr><td colSpan="2">Blank Table {i+1}</td></tr></thead>
-                <tbody>{rows}</tbody>
-                </table>);
-        }
-        return tableList;
+        const title = "Blank Table " + (this.props.tableID +1);
+        let newTable = <div className="tableContainer">
+            <div className="table"><table key = {this.props.tableID}>
+            <thead>
+                <tr>
+                    <td colSpan="2">
+                        <form onSubmit={this.handleTitleSave}>
+                            <input type="text" placeholder={title} value = {this.state.tableTitle}  onChange = {this.handleTitleChange} />    
+                        </form></td>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+        </div>
+        </div>;
+
+        return newTable;
     }
 }
 
